5d6a41962ffa671271edaad925697e7a353e2a03
M-Eleoterio <m.eleoteriovieira@gmail.com>
2024-09-09 20:59:36 -0300

    :feat add cards btn to projects

--- src/components/prjCard.jsx
+++ src/components/prjCard.jsx
@@ -1,5 +1,6 @@
 import '../style/prjCard.css';
 import { Link } from 'react-router-dom';
+import { FaGithub } from "react-icons/fa";
 
 export default function PrjCard(props) {
     return (
@@ -7,6 +8,11 @@ export default function PrjCard(props) {
             <img className='card-img' src={props.img || "https://placehold.jp/500x300.png"} alt="" />
             <h1 className="card-title">{props.title || "Project Name"}</h1>
             <p className="card-desc">{props.desc || "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio reprehenderit, veritatis voluptatum temporibus eaque sapiente, ad sequi labore, nobis numquam maxime harum tenetur architecto. Cupiditate ipsum et mollitia illum maiores."} </p>
+            <Link to={props.link || "https://github.com/m-eleoterio"} target='_blank' className="card-btn">
+            <FaGithub /> Go to project &#x2192;
+            </Link>
         </div>
+
+
     );
 }
